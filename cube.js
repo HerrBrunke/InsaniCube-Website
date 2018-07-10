f4ae4808eeceb067763f4b1df109a60980493284
HerrBrunke <34299221+HerrBrunke@users.noreply.github.com>
2018-07-10 14:00:04 +0200

    CubneProtoBuggy
    asf

--- cube.js
+++ cube.js
@@ -1,18 +1,58 @@
 var cubeModel;
+var rotateAngle;
+var formerMouseX;
+var formerMouseY;
+var transX = 0;
+var transY = 0;
+var vectorX = createVector(1, 0, 0);
+var vectorY = createVector(0, 1, 0); 
+function setup() {
+	createCanvas(windowWidth, windowHeight, WEBGL);
 
+}
 function preload() {
 	
-	cubeModel = loadModel(assets/cube.obj, normalize)
-}
-
-function setup() {
-	createCanvas(windowWidth / 2, windowHeight / 2);
-	canvas.parent('cube');
-
+	cubeModel = loadModel('assets/cube.obj');
 }
 
 function draw() {
-	background(225);
-	cubeModel.draw();
+	background(250);
+	scale(30);
+	rotate(transX, vectorX);
+	rotate(transY, vectorY);
+	model(cubeModel);
+
+	//while mouseispressed look if mousePresseSetPoint is higher or lower, so rotate the cube.
+	if(mouseIsPressed){
+		console.log(mouseX, mouseY);
+		if(formerMouseX < mouseX)
+		{
+			transX = transX - 0.5;
+		}
+		else if(formerMouseX > mouseX)
+		{
+			transX = transX + 0.5;
+		}
+		
+		if(formerMouseY < mouseY)
+		{
+			transY = transY - 0.5;
+		}
+		else if(formerMouseY > mouseY)
+		{
+			transY = transY + 0.5;
+		}
+	}
+	
 }
+//if mousePressed set new startlocation
+function mousePressed(){
+	//reset translation
+	//transX = 0;
+	//transY = 0;
+	console.log("mousepressedOnce!", formerMouseX, formerMouseY);
+	formerMouseX = mouseX;
+	formerMouseY = mouseY;
+}
+
 
